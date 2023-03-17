a83a57d0ce6f25502b67b539b5d1764620a87ea4
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2023-03-17 18:23:31 -0300

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+
+  const [nav, setNav] = useState();
+
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "portfolio",
+    },
+    {
+      id: 4,
+      link: "experience",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
         <h1 className="text-5xl font-signature ml-2">Cortinhas</h1>
       </div>
 
-      <ul className="flex">
-        <li>Home</li>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
+            {link}
+          </li>
+        ))}
       </ul>
+
+      <div className="cursor-pointer pr-4 z-10 text-gray-500">
+        <FaBars size={30} />
+      </div>
+
     </div>
   );
 };
